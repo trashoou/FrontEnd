d450492459a7a0b6df3490f0d73bd2e266af826a
trashoou <dima2002zxz@gmail.com>
2023-11-22 19:39:26 +0100

    update

---
+++ lesson_08/js-intro/index.js
@@ -0,0 +1,18 @@
+// вывод в консоль
+console.log("chek 1 2")
+
+// Создание переменных
+// let, var, const
+let dog = "Sharik";
+dog = "Tuzik";
+dog = 12; // изменили тип данных
+console.log(dog)
+
+const car = "Barsik";
+
+
+// Примитивные типы данных
+// 1. string - является строкой/притивом
+
+let str1 = "Можно в двойных"
+let str2 = 'Можно в одинарных'
