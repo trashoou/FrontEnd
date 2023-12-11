7fc329ceb7cd15cdedeef3c87fd2b4b37500e0b6
trashoou <dima2002zxz@gmail.com>
2023-12-11 13:44:01 +0100

    update

--- lesson_14/hw/script.js
+++ lesson_14/hw/script.js
@@ -1,8 +1,18 @@
 async function fetchData() {
     try {
-        const response = await fetch(`./data.json`);
+        const response = await fetch(`https://raw.githubusercontent.com/trashoou/FrontEnd/main/lesson_14/hw/index.json`);
         const data = await response.json();
+
         console.log(data);
+        
+        const ul = document.createElement('ul');
+        data.array.forEach(element => {
+            const li = document.createElement('li');
+            li.textContent = `Name: ${element.name}, Age: ${element.age}`;
+            ul.appendChild(li);
+        });
+
+        document.body.appendChild(ul);
     } catch (error){
         console.error("Ошибка загрузки данных: ", error);
     }
